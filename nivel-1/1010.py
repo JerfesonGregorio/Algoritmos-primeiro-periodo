3c4cb9e7173b53fc579de32f4b1c60b50f3608ca
JerfesonGregorio <pro.jerfeson@gmail.com>
2024-03-04 16:11:17 -0300

    atualizacao de questoes

---
+++ nivel-1/1010.py
@@ -0,0 +1,21 @@
+'''
+Neste problema, deve-se ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. Após, calcule e mostre o valor a ser pago.
+
+Entrada
+O arquivo de entrada contém duas linhas de dados. Em cada linha haverá 3 valores, respectivamente dois inteiros e um valor com 2 casas decimais.
+
+Saída
+A saída deverá ser uma mensagem conforme o exemplo fornecido abaixo, lembrando de deixar um espaço após os dois pontos e um espaço após o "R$". O valor deverá ser apresentado com 2 casas após o ponto.
+'''
+
+vls1 = input().split()
+vls2 = input().split()
+
+c1, n1, v1 = int(vls1[0]), int(vls1[1]), float(vls1[2])
+c2, n2, v2 = int(vls2[0]), int(vls2[1]), float(vls2[2])
+
+
+
+print() 
+
+
